11819f156ccf7e4d9d31655b875880519982ace3
Eric Lin <eric.lin@nyu.edu>
2017-04-22 14:46:10 -0400

    Implemented stepper.go

--- stepper.go
+++ stepper.go
@@ -8,3 +8,74 @@
 //
 
 package roombaControl
+
+import (
+	"fmt"
+	"time"
+	"github.com/stianeikeland/go-rpio"
+)
+
+type Stepper struct {
+	pin_step Pin
+	delayMillisecond int
+	pin_microstep1 Pin
+	pin_microstep2 Pin
+	pin_microstep3 Pin
+}
+
+func (s Stepper) step() {
+	s.pin_step.Output()
+	s.pin_step.High()
+	time.Sleep(delayMillisecond * time.Millisecond)
+	s.pin_step.Low()
+	time.Sleep(delayMillisecond * time.Millisecond)
+}
+
+func (s Stepper) set_full_step() {
+	s.pin_microstep1.Output()
+	s.pin_microstep1.Low()
+	s.pin_microstep2.Output()
+	s.pin_microstep2.Low()
+	s.pin_microstep3.Output()
+	s.pin_microstep3.Low()
+}
+
+func (s Stepper) set_half_step() {
+	s.pin_microstep1.Output()
+	s.pin_microstep1.High()
+	s.pin_microstep2.Output()
+	s.pin_microstep2.Low()
+	s.pin_microstep3.Output()
+	s.pin_microstep3.Low()
+}
+
+func (s Stepper) set_quater_step() {
+	s.pin_microstep1.Output()
+	s.pin_microstep1.Low()
+	s.pin_microstep2.Output()
+	s.pin_microstep2.High()
+	s.pin_microstep3.Output()
+	s.pin_microstep3.Low()
+}
+
+func (s Stepper) set_eighth_step() {
+	s.pin_microstep1.Output()
+	s.pin_microstep1.High()
+	s.pin_microstep2.Output()
+	s.pin_microstep2.High()
+	s.pin_microstep3.Output()
+	s.pin_microstep3.Low()
+}
+
+func (s Stepper) set_sixteenth_step() {
+	s.pin_microstep1.Output()
+	s.pin_microstep1.High()
+	s.pin_microstep2.Output()
+	s.pin_microstep2.High()
+	s.pin_microstep3.Output()
+	s.pin_microstep3.High()
+}
+
+func initializeStepper(pin_step int, delayMillisecond int, pin_microstep1 int, pin_microstep2 int, pin_microstep3 int) *Stepper {
+	return Stepper{rpio.Pin(pin_step), delayMillisecond, rpio.Pin(pin_microstep1), pio.Pin(pin_microstep2), pio.Pin(pin_microstep3)}
+}
